c0febd3556ac22b056f3d506ed626539eb1b6c7c
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-01-28 21:05:07 +0100

    new style

--- script/mainScript.js
+++ script/mainScript.js
@@ -4,13 +4,6 @@ $(document).ready(function () {
   // Side nav
   $('.button-collapse').sideNav();
 
-  //  slider
-  $('.slider').slider({
-    indicators: false,
-    height: 350,
-    transition: 500,
-    interval: 5000
-  })
 
 
 
